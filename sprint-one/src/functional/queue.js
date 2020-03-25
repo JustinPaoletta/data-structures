ee913e39aabea92828e8476b518807c4623ae2ee
Justin Paoletta <justinpaoletta@gmail.com>
2020-03-25 18:31:18 -0400

    finished functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,33 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    var index = Object.keys(storage).length;
+    if (storage[index] === undefined) {
+      storage[index] = value;
+    }
   };
 
+
+  // it('removes the oldest item, after newer items have already been added and removed', function() {
+  //   queue.enqueue('a');
+  //   queue.enqueue('b');
+  //   queue.dequeue();
+  //   queue.enqueue('c');
+  //   expect(queue.dequeue()).to.equal('b');
+  // });
+
+  // {0 : "a", 1 : "b"};
   someInstance.dequeue = function() {
+    var index = Object.keys(storage);
+    var firstIn = storage[index[0]];
+    delete storage[index[0]];
+    return firstIn;
   };
 
   someInstance.size = function() {
+    var index = Object.keys(storage).length;
+    return index;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,32 @@
 var Stack = function() {
+
   var someInstance = {};
 
   // Use an object with numeric keys to store values
+
   var storage = {};
 
   // Implement the methods below
+
   someInstance.push = function(value) {
+    var index = Object.keys(storage).length;
+    if (storage[index] === undefined) {
+      storage[index] = value;
+    }
+
   };
 
   someInstance.pop = function() {
+    var index = Object.keys(storage).length - 1;
+    var last = storage[index];
+    delete storage[index];
+    console.log(Object.keys(storage));
+    return last;
   };
 
   someInstance.size = function() {
+    var index = Object.keys(storage).length;
+    return index;
   };
 
   return someInstance;
