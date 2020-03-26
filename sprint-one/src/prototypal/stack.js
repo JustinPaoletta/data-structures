e9baaa16e01da96b5d0d80520b8d2972ce008b69
Justin Paoletta <justinpaoletta@gmail.com>
2020-03-26 13:19:30 -0400

    functional shared fixed

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,13 @@
-var Stack = function() {
+var Stack = function(method1, method2, method3) {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.method1 = method1;
+  this.method2 = method2;
+  this.method3 = method3;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+};
 
 
