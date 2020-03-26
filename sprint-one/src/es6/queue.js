9128dd7053a0b465134f707835b723f35aaa5b9b
Justin Paoletta <justinpaoletta@gmail.com>
2020-03-26 19:56:20 -0400

    finished

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,26 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    Object.create(this);
+  }
+
+  enqueue(value){
+    var index = Object.keys(this).length;
+    if (!this[index]) {
+      this[index] = value;
+    }
+  }
+
+  dequeue(){
+    var index = Object.keys(this);
+    var firstProp = this[index[0]];
+    delete this[index[0]];
+    return firstProp;
+  }
+
+  size(){
+    var stackSize = Object.values(this).length;
+    return stackSize;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
-  }
+  	Object.create(this);
+  };
+
+  push(Value) {
+  	let index = Object.keys(this).length;
+  		if (!this[index]) {
+    		this[index] = Value;
+  		}
+  };
+
+  pop() {
+  	let index = Object.keys(this);
+  	let last = this[index.length - 1];
+  	delete this[index.length - 1];
+  	return last;
+  };
+
+  size () {
+  	let size = Object.keys(this).length;
+  	return size;
+  };
+
 
 }
